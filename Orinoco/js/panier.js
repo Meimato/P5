851f2d469ec5bdcd6882c38bc47f3f511b2508a2
Gabriele Ferro <gabrielluzzo@gmail.com>
2020-08-31 03:26:47 +0200

    Redesign Product Detail Page

--- Orinoco/js/panier.js
+++ Orinoco/js/panier.js
@@ -1,17 +1,14 @@
 var request = new XMLHttpRequest();
 
 request.onreadystatechange = function () {
-
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-
-        const myCartElement = document.getElementById("panier");
-        const newBox = document.createElement("div");
-        const s = localStorage.getArray("AddedToCart");
-        myCartElement.appendChild(newBox);
-        console.log(s);
-    }
-
-}
+  if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+    const myCartElement = document.getElementById("panier");
+    const newBox = document.createElement("div");
+    const s = localStorage.getArray("AddedToCart");
+    myCartElement.appendChild(newBox);
+    console.log(s);
+  }
+};
 
 request.open("GET", "http://localhost:3000/api/cameras");
 request.send();
