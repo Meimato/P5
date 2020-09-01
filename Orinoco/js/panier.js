6fddbc1b23da8b15cf7adca35e7eb7b082da3320
Gabriele Ferro <gabrielluzzo@gmail.com>
2020-09-02 01:48:46 +0200

    Barebone Cart display total in the console. No CSS

--- Orinoco/js/panier.js
+++ Orinoco/js/panier.js
@@ -2,11 +2,23 @@ var request = new XMLHttpRequest();
 
 request.onreadystatechange = function () {
   if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-    const myCartElement = document.getElementById("panier");
-    const newBox = document.createElement("div");
-    const s = localStorage.getArray("AddedToCart");
-    myCartElement.appendChild(newBox);
-    console.log(s);
+    const myCartTable = document.getElementById("panier");
+    const myCart = localStorage.getArray("AddedToCart");
+    let total = 0;
+    for (let i = 0; i < myCart.length; i++ ) {
+      let tr = document.createElement("tr");
+      let td = document.createElement("td");
+      let td2 = document.createElement("td");
+      let productName = document.createTextNode(myCart[i].name);
+      let productPrice = document.createTextNode(myCart[i].price);
+      td.appendChild(productName);
+      td2.appendChild(productPrice);
+      tr.appendChild(td);
+      tr.appendChild(td2);
+      myCartTable.appendChild(tr);
+      total += myCart[i].price;
+    }
+    console.log("The total is: " + total + "€");
   }
 };
 
